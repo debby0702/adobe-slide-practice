d45f28ffa1ac8ff4ab84e0f91d8dfe7c22fff1db
Debby Yang <dla070284@gmail.com>
2024-07-01 20:25:28 +0800

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -1,7 +1,9 @@
+// 全域變數-------------------------------
 const $nav = $('#Nav');
 // html 把 #Grid改為 #Slide
-const $slide= $('#Slide');
-const $grid= $slide.find(".grid");
+const $slide = $('#Slide');
+const $grid = $slide.find(".grid");
+const $btns = $nav.find('.nav-btn');
 
 console.log($grid)
 
@@ -16,7 +18,7 @@ console.log($grid)
 
 // 方法二：表達式-----------------------------------
 // 判斷是否是手機版(F12看console會出現true/ flase)
-const isMobile = $(window).width()<=480;
+const isMobile = $(window).width() <= 480;
 
 // 抓出可是範圍寬度(高度)
 // window 不是標籤，不用引號
@@ -25,7 +27,7 @@ console.log(isMobile)
 // console.log($(window).height())
 
 
-function setGrid(){
+function setGrid() {
     // 這裡要用2個or3個=
     // if (isMobile==true){
     // 但其實不用寫==true(優化)
@@ -36,7 +38,7 @@ function setGrid(){
     // }
 
     // 優化寫法(純粹減少大括號)
-    if (isMobile){
+    if (isMobile) {
         $grid.addClass('grid-2')
         return;
         // 停下的意思，不加做grid-3
@@ -63,76 +65,156 @@ setGrid();
 //     }
 // });
 
-function setScroll(){
-    $(window).scroll(function(){
+function setScroll() {
+    $(window).scroll(function () {
         // isMobile: true
         // !isMobile:flase
-        if(!isMobile) return;
-        
-        
-        if($(this).scrollTop() == 0){
+        if (!isMobile) return;
+
+        if ($(this).scrollTop() == 0) {
             // console.log(0)
             $nav.removeClass('nav-active')
-        }else{
+        } else {
             // console.log('要套用class')
             $nav.addClass('nav-active')
         }
     })
 }
 
-function setFancybox(){
+function setFancybox() {
     $grid.find("a").fancybox({
         // 防按右鍵被下載
         protect: true,
         // 讓圖片循環(最後跳回第一張)
         loop: true,
         // 開啟、關閉時間
-        animationDuration:1000,
+        animationDuration: 1000,
         // 轉場時間(已經打開了)
-        transitionDuration:2000,
+        transitionDuration: 2000,
         // 轉場效果 (滑鼠拖曳/手機/鍵盤/按鈕點擊都支援)
-        transitionEffect:'slide',
+        transitionEffect: 'slide',
         // 全螢幕，預設false
         // fullScreen:{
         //     autoStart:true,
         // },
         // 是否鎖住鍵盤操作，預設是true
-        keyboard:false,
+        keyboard: false,
         // 分頁顯示，預設true
-        infobar:false,
+        infobar: false,
         // 顯示工具列，預設true
         // toolbar:true,
         // 顯示圖片旁的關閉鈕，預設false
-        smallBtn:true,
+        smallBtn: true,
         // 是否顯示箭頭按鈕，預設true
-        arrows:false,
+        arrows: false,
         // 自動轉場
-        slideShow:{
-            autoStart:true,
-            speed:5000,
+        slideShow: {
+            autoStart: true,
+            speed: 5000,
         },
         // 顯示縮圖模式，預設true
-        thumbs:true,
+        thumbs: true,
         // 媒體自動播放
-        media:{
-            youtube:{
-                params:{
-                    autoplay:false
+        media: {
+            youtube: {
+                params: {
+                    autoplay: false
                 }
             },
             // 除了YT，其他頻台影片也能操控
         }
     });
-    
+
 }
 
-function setInit(){
+// 按nav按鈕就會移動到相對應位置
+function setClickBtn() {
+    $btns.click(function () {
+        // alert(3);
+        
+        // 避免記憶體外洩(使用者電腦越跑越慢)
+        // 當事者，啟用'disebled(不運作)功能'，其他鈕關閉此功能
+        // (補充)關於disabled，input也是這樣做不運作的按鈕。
+        $(this)
+            .attr('disabled', true)
+            .siblings().attr('disabled', false);
+        
+            // 區域變數-------------------------------
+        const index = $(this).index()
+        // console.log(index)
+
+        // 滑動效果詳見css的.grid-slide
+        // 全用if分開寫(浪費效能，全部都會執行一遍)
+        // if(index == 0){
+        //     $slide.css('transform','translate(0,0)');
+        // }
+        // if(index == 1){
+        //     $slide.css('transform','translate(-100vw,0)');
+        // }
+        // if(index == 2){
+        //     $slide.css('transform','translate(-200vw,0)');
+        // }
+        // if(index == 3){
+        //     $slide.css('transform','translate(0,-100vh)');
+        // }
+        // if(index == 4){
+        //     $slide.css('transform','translate(-100vw,-100vh)');
+        // }
+        // if(index == 5){
+        //     $slide.css('transform','translate(-200vw,-100vh)');
+        // }
+
+        // else if 為6選1執行(節省效能)
+        // if(index == 0){
+        //     $slide.css('transform','translate(0,0)');
+        // }else if(index == 1){
+        //     $slide.css('transform','translate(-100vw,0)');
+        // }else if(index == 2){
+        //     $slide.css('transform','translate(-200vw,0)');
+        // }else if(index == 3){
+        //     $slide.css('transform','translate(0,-100vh)');
+        // }else if(index == 4){
+        //     $slide.css('transform','translate(-100vw,-100vh)');
+        // }else if(index == 5){
+        //     $slide.css('transform','translate(-200vw,-100vh)');
+        // }
+
+        // switch是最不耗效能的(要搭配break)
+        switch (index) {
+            // case index值:要做的事
+            case 0:
+                $slide.css('transform', 'translate(0,0)');
+                break;
+            case 1:
+                $slide.css('transform', 'translate(-100vw,0)');
+                break;
+            case 2:
+                $slide.css('transform', 'translate(-200vw,0)');
+                break;
+            case 3:
+                $slide.css('transform', 'translate(0,-100vh)');
+                break;
+            case 4:
+                $slide.css('transform', 'translate(-100vw,-100vh)');
+                break;
+            case 5:
+                $slide.css('transform', 'translate(-200vw,-100vh)');
+                break;
+        }
+
+    })
+
+}
+
+function setInit() {
     setGrid();
+    $btns.eq(0).attr('disabled',true)
 }
 
-function setEvent(){
+function setEvent() {
     setScroll();
     setFancybox();
+    setClickBtn();
 }
 
 
